2fbf64c5d8df9e49cce8aff46133119154a10759
Kineruu <Kineru08@gmail.com>
2026-09-04 19:57:41 +0200

    ESP32 Buttons code

--- esp.py
+++ esp.py
@@ -7,37 +7,34 @@ import time
 # make it either player vs bot or player vs player
 # maybe document the entire thing like what i did and what parts i used
 
-# Left Paddle
-L_buttons = [
-    Pin(1, Pin.IN, Pin.PULL_UP),
-    Pin(2, Pin.IN, Pin.PULL_UP),
-    Pin(44, Pin.IN, Pin.PULL_UP),
-    Pin(43, Pin.IN, Pin.PULL_UP)
-]
+info = 0
 
-Settings_buttons = [
-    #Pin() Quit
-    #Pin() Stop? Pause?
-    #Pin() Player vs Player
-    #Pin() Player vs Bot
-    #Pin() Bot vs Bot?  
-]
+# Left Paddle
+b1 = Pin(11, Pin.IN, Pin.PULL_UP) # W
+b2 = Pin(14, Pin.IN, Pin.PULL_UP) # A
+b3 = Pin(13, Pin.IN, Pin.PULL_UP) # S
+b4 = Pin(12, Pin.IN, Pin.PULL_UP) # D
 
 # Right paddle
-R_buttons = [
-    #Pin(),
-    #Pin(),
-    #Pin(),
-    #Pin()
-]
+r1 = Pin(4, Pin.IN, Pin.PULL_UP) # ↑
+r2 = Pin(5, Pin.IN, Pin.PULL_UP) # ←
+r3 = Pin(7, Pin.IN, Pin.PULL_UP) # ↓
+r4 = Pin(6, Pin.IN, Pin.PULL_UP) # →
 
 while True:
-    L_values = [button.value() for button in L_buttons]
-    #R_values = [button.value() for button in R_buttons]
+    if info == 1:
+        print("---------------")
+        print("L1: ", b1.value()) # W
+        print("L2: ", b2.value()) # A
+        print("L3: ", b3.value()) # S
+        print("L4: ", b4.value()) # D
+        print("---------------")
+        print("R1: ", r1.value()) # ↑
+        print("R2: ", r2.value()) # ←
+        print("R3: ", r3.value()) # ↓
+        print("R4: ", r4.value()) # →
 
-    print("Left: ", L_values)
-    #print("Right:", R_values)
+    
 
-    time.sleep_ms(250)
-
+    time.sleep_ms(100)
     
